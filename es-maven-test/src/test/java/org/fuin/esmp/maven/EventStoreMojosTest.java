d35c52f1a9c8e9172686066373ce1268b6ac6927
Michael Schnell <michael@fuin.org>
2015-05-23 14:25:48 +0200

    Fixed wrong package name

--- es-maven-test/src/test/java/org/fuin/esmp/maven/EventStoreMojosTest.java
+++ es-maven-test/src/test/java/org/fuin/esmp/maven/EventStoreMojosTest.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.fuin.srcgen4j.maven;
+package org.fuin.esmp.maven;
 
 import java.io.File;
 import java.util.ArrayList;
