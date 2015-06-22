1d2189a4340699438013baa589ae1568f7b2c4ca
Michael Schnell <michael@fuin.org>
2015-06-22 20:16:37 +0200

    Added test

---
+++ es-maven-plugin/src/test/java/org/fuin/esmp/EventStorePostStartMojoTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2015 Michael Schnell. All rights reserved. 
+ * <http://www.fuin.org/>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.fuin.esmp;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.exec.OS;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Test for {@link EventStorePostStartMojo}.
+ */
+public class EventStorePostStartMojoTest {
+
+    // CHECKSTYLE:OFF Test
+    
+    @Ignore("Fix test!")
+    @Test
+    public void testExecute() throws MojoExecutionException, IOException {
+        
+        // PREPARE
+        final EventStorePostStartMojo testee = new EventStorePostStartMojo();
+        final File dir = new File("./src/test").getCanonicalFile();
+        testee.setEventStoreDir(dir);
+        if (OS.isFamilyWindows()) {
+            testee.setPostStartCommand(dir + File.separator + "echotest.bat");
+        } else {
+            // TODO Fails with error #13 Not authorized
+            testee.setPostStartCommand(dir + File.separator + "echotest.sh");
+        }
+        
+        // TEST
+        testee.execute();        
+        
+        // VERIFY        
+        assertThat(testee.getMessages()).contains("bla");
+        
+    }
+    
+    // CHECKSTYLE:ON
+    
+}
